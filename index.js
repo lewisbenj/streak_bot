c46e80c00635ea6983ec6d60cb78a5c179efe001
Nguyễn Võ Chí Nguyện <nguyenchie60@gmail.com>
2025-08-18 19:21:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -194,3 +194,16 @@ client.on("messageCreate", async (message) => {
 });
 
 client.login(process.env.TOKEN);
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
